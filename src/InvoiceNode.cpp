fb09714d26651a0e92b8998e6965523f5beb9e8e
Alfredo-Vargas <alfredo.vargas-paredes@outlook.com>
2021-11-25 09:34:30 +0100

    two options for the localtime function

--- src/InvoiceNode.cpp
+++ src/InvoiceNode.cpp
@@ -19,10 +19,26 @@ void InvoiceNode::setDate(void)		// this function requires the ctime header
 {
 	// idea taken from: https://www.tutorialspoint.com/cplusplus/cpp_date_time.htm
 	// localtime was deprecated, he we used localtime_s instead
-	time_t now = time(0);
+
+	/* DATE IMPLEMENTATION FOR WINDOWS (uses localtime_s)
+	time_t now = time(0);  // returns current time in seconds
 	struct tm ltm;
 	localtime_s(&ltm, &now);
 	// tm* ltm = localtime(&now);
+	date.setYear(1900 + ltm.tm_year);
+	date.setMonth(1 + ltm.tm_mon);
+	date.setDay(ltm.tm_mday);
+	date.setHour(5 + ltm.tm_hour);
+	date.setMinute(30 + ltm.tm_min);
+	date.setSecond(ltm.tm_sec);
+	*/
+
+
+	// DATE IMPLEMENTATION FOR LINUX (uses localtime_r)
+	time_t now = time(0);	// return current time in seconds
+	struct tm ltm;
+
+	localtime_r(&now, &ltm);
 	date.setYear(1900 + ltm.tm_year);
 	date.setMonth(1 + ltm.tm_mon);
 	date.setDay(ltm.tm_mday);
